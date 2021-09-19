02b7f18f8d3e6c7eb03454333ef0dccdf247ebcf
2020 <fyodor.kazaryan@bk.ru>
2021-09-19 21:29:45 +0300

    created Printer-class for templating, not done yet

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 require_once dirname(__FILE__) . '/classes/User.php';
+require_once dirname(__FILE__) . '/classes/Printer.php';
 
 $user = new User();
 if (isset($_GET['out']))
     if ($_GET['out'] == 1)
         $user->out();
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/index/top.html';
+Printer::index('top');
 
 if (isset($_SESSION['is_auth']) && $_SESSION['is_auth']) {
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/index/is-auth.html';
+    Printer::index('is-auth');
 } else {
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/index/is-not-auth.html';
+    Printer::index('is-not-auth');
 }
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/index/header-bottom.html';
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/index/main.html';
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/templates/index/footer.html';
+Printer::index('header-bottom');
+Printer::index('main');
+Printer::index('footer');
 
 
